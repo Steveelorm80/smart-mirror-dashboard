4ceda19632f22ff2fd679b4762cfa70435599fc3
Stephen Kwaku Pometsey <steveelorm@gmail.com>
2026-03-05 18:33:34 +0000

    refactor project structure to client/server architecture

--- config/config.js
+++
@@ -1,7 +0,0 @@
-export const getConfig = () => {
-  return {
-    weatherApiKey: process.env.WEATHER_API_KEY,
-    newsApiKey: process.env.NEWS_API_KEY,
-    openaiApiKey: process.env.OPENAI_API_KEY,
-  };
-};
